f47caff6ff0827ca5f9949aaeaf879f66e5e417e
Adriana Olindo <adriana-olindo18@hotmail.com>
2024-04-26 18:40:18 -0300

    Projeto finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do titular: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o numero da agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o numero da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("informe o valor a depositar: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá sr(a) " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia é "+ agencia + " , conta "+ numero +" e seu saldo "+ saldo + " já está disponivel para saque!");
+}
+
 }
